664f06c2472f345af3688e7de221b619e0e880a2
elliotAlderson <marco.caballero.d@gmail.com>
2018-01-28 18:25:29 +0100

    Refactoring quality smell codes II

--- testlink-plugin/src/main/java/com/marco/tlp/controllers/TestCaseRestController.java
+++ testlink-plugin/src/main/java/com/marco/tlp/controllers/TestCaseRestController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
