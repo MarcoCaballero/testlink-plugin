ba68f725ec14c9ceea24a818d306c3f5720c8b01
MarcoCab <marco.caballero.d@gmail.com>
2018-04-13 19:26:21 +0200

    refactoring deployment scripts and deployment to stable version all version (1.0.0)  [--deploy-DockerHub]

--- docker/deploy_dev.sh
+++ docker/deploy_dev.sh
@@ -2,14 +2,15 @@
 VERSION=$(cat docker/VERSION)
 
 echo "tagging frontend with :dev, and :$VERSION"
-docker tag marcocab/testlink-plugin-gui marcocab/testlink-plugin-gui:dev
-docker tag marcocab/testlink-plugin-gui marcocab/testlink-plugin-gui:$VERSION
+docker tag marcocab/testlink-plugin-gui:dev marcocab/testlink-plugin-gui:$VERSION
+
 echo "tagging backend with :dev, and :$VERSION"
-docker tag marcocab/testlink-plugin-rest marcocab/testlink-plugin-rest:dev
-docker tag marcocab/testlink-plugin-rest marcocab/testlink-plugin-rest:$VERSION
+docker tag marcocab/testlink-plugin-rest:$VERSION marcocab/testlink-plugin-rest:dev
+
 echo "pushing frontend with :dev, and :$VERSION"
 docker push marcocab/testlink-plugin-gui:dev
 docker push marcocab/testlink-plugin-gui:$VERSION
+
 echo "pushing backend with :dev, and :$VERSION"
 docker push marcocab/testlink-plugin-rest:dev
 docker push marcocab/testlink-plugin-rest:$VERSION
