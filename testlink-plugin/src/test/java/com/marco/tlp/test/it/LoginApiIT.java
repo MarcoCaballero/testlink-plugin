a75a95f16a4d5a71efb5946b15ef5a5731b680a1
MarcoCab <marco.caballero.d@gmail.com>
2018-06-06 09:45:05 +0200

    Typos fixing.

--- testlink-plugin/src/test/java/com/marco/tlp/test/it/LoginApiIT.java
+++ testlink-plugin/src/test/java/com/marco/tlp/test/it/LoginApiIT.java
@@ -13,15 +13,15 @@ public class LoginApiIT {
 
 	@Test
 	public void loginSuccessTest() {
-		given().headers("TLP-Server-Url", TESTLINK_SERVER_URL, "TLP-Api-Key", API_KEY_GOOD).when()
-				.get("/tlp-api/authorization").then().statusCode(200).body("url", equalTo(TESTLINK_SERVER_URL))
-				.body("result", equalTo("true"));
+		given().headers("TLP-Server-Url", TESTLINK_SERVER_URL, "TLP-Api-Key", API_KEY_GOOD)
+			.when().get("/tlp-api/authorization")
+			.then().statusCode(200).body("url", equalTo(TESTLINK_SERVER_URL)).body("result", equalTo("true"));
 	}
 
 	@Test
 	public void loginFailTest() {
-		given().headers("TLP-Server-Url", TESTLINK_SERVER_URL, "TLP-Api-Key", API_KEY_BAD).when()
-				.get("/tlp-api/authorization").then().statusCode(401).body("url", equalTo(TESTLINK_SERVER_URL))
-				.body("result", equalTo("false"));
+		given().headers("TLP-Server-Url", TESTLINK_SERVER_URL, "TLP-Api-Key", API_KEY_BAD)
+			.when().get("/tlp-api/authorization")
+			.then().statusCode(401).body("url", equalTo(TESTLINK_SERVER_URL)).body("result", equalTo("false"));
 	}
 }
